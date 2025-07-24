11f4a3d5ebfc844f7b420b9fb3e633d969387356
prajakta <prajaktaks2004@gmail.com>
2025-07-24 15:36:59 +0530

    first commit

---
+++ CAL.groovy
@@ -0,0 +1,22 @@
+pipeline{
+    agent any 
+   stages{ 
+    stage('clone code') {
+        steps{ 
+            git branch: 'main', url: 'https://github.com/Prajakta18-boop/calyx.git'
+            }
+            
+    }
+    stage('deploying code') {
+        steps{
+            sh 'sudo mv /var/lib/jenkins/workspace/agentB/index.html /var/www/html/CALYX/CALYX'
+        }
+    }   
+    stage ('restarting nginx'){
+        steps{
+           sh 'sudo systemctl restart nginx'
+        }
+    }
+   }
+
+}
